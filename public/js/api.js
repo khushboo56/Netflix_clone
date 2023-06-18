b7b8c84ecbeac94978b1031b244e00658978eff6
Khushboo <kgupta8646@gmail.com>
2023-06-18 23:50:47 +0530

    update

--- public/js/api.js
+++ public/js/api.js
@@ -1,5 +1,5 @@
 let api_key = "84be25f0ca8598922d57ea2e3d73db7fs";
 
-let img_url = "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg";
+let img_url = "https://image.tmdb.org/t/p/w500";
 let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
 let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
