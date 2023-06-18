aea4d0306fae39bb38622b95fd2248e7415f54ef
Khushboo <kgupta8646@gmail.com>
2023-06-18 23:48:50 +0530

    update

--- public/js/api.js
+++ public/js/api.js
@@ -1,7 +1,5 @@
-let api_key = "84be25f0ca8598922d57ea2e3d73db7f";
+let api_key = "84be25f0ca8598922d57ea2e3d73db7fs";
 
-let img_url = "https://image.tmdb.org/t/p/w500";
-let original_img_url = "https://image.tmdb.org/t/p/original";
+let img_url = "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg";
 let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
 let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
-let movie_detail_http = "https://api.themoviedb.org/3/movie";
