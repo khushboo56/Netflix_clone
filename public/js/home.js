b621cc4211a5c20fa85bec243d7f27b51afcd8d1
Khushboo <kgupta8646@gmail.com>
2023-06-18 23:01:44 +0530

    update

---
+++ public/js/home.js
@@ -0,0 +1,9 @@
+fetch(genres_list_http + new URLSearchParams({
+    api_key: api_key
+}))
+.then(res => res.json())
+.then(data => {
+    data.genres.forEach(item => {
+        fetchMoviesListByGenres(item.id, item.name);
+    })
+});
